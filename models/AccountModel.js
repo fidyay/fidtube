5c95625575e6934e775a4fc4cf09469e88ab16ea
Fidyay <fidiy1878@gmail.com>
2023-08-26 15:55:00 +0300

    fixed tooltip position

--- models/AccountModel.js
+++ models/AccountModel.js
@@ -1,29 +1,30 @@
-const mongoose = require('mongoose')
-const { Schema, model } = mongoose 
+/* eslint-disable @typescript-eslint/no-var-requires */
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
 
 const accountSchema = Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    avatar: {
-        type: String,
-        default: null
-    },
-    description: {
-        type: String,
-        default: null
-    },
-    subscribtions: {
-        type: [Schema.Types.ObjectId],
-    },
-})
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    default: null,
+  },
+  description: {
+    type: String,
+    default: null,
+  },
+  subscribtions: {
+    type: [Schema.Types.ObjectId],
+  },
+});
 
-const AccountModel = model('Account', accountSchema)
+const AccountModel = model("Account", accountSchema);
 
-module.exports = AccountModel
+module.exports = AccountModel;
--- models/VideoModel.js
+++ models/VideoModel.js
@@ -1,46 +1,46 @@
-const mongoose = require('mongoose')
-const { Schema, model } = mongoose 
-
+/* eslint-disable @typescript-eslint/no-var-requires */
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
 
 const videoSchema = Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        default: null
-    },
-    comments: {
-        type: Array,
-        default: []
-    },
-    likes: {
-        type: Number,
-        default: 0
-    },
-    dislikes: {
-        type: Number,
-        default: 0
-    },
-    author: { 
-        type: Schema.Types.ObjectId,
-        ref: 'Account'
-    },
-    source: {
-        type: String,
-        default: null
-    },
-    votedBy: {
-        type: Array,
-        default: []
-    },
-    preview: {
-        type: String,
-        default: null
-    }
-})
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    default: null,
+  },
+  comments: {
+    type: Array,
+    default: [],
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  dislikes: {
+    type: Number,
+    default: 0,
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "Account",
+  },
+  source: {
+    type: String,
+    default: null,
+  },
+  votedBy: {
+    type: Array,
+    default: [],
+  },
+  preview: {
+    type: String,
+    default: null,
+  },
+});
 
-const VideoModel = model('Video', videoSchema)
+const VideoModel = model("Video", videoSchema);
 
-module.exports = VideoModel
+module.exports = VideoModel;
